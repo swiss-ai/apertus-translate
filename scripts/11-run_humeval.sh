3cc5f9286235ee8474aa565f6fd666cd548ebc87
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-07-05 13:51:14 +0200

    update launch script

--- scripts/11-run_humeval.sh
+++ scripts/11-run_humeval.sh
@@ -3,5 +3,5 @@ pearmut add campaigns/*.json
 
 pearmut add campaigns/{German-Italian,German-French,German-English,English-Italian,English-French,English-German,Italian-German,Italian-French,Italian-English,French-German,French-Italian,French-English}.json -o
 
-ngrok http 8003 --url=pearmut.ngrok.dev --traffic-policy-file=/home/vilda/pearmut/misc/policy.yml
-pearmut run --port 8003 --url https://pearmut.ngrok.dev
+ngrok http 8003 --url=pearmut.ngrok.io --traffic-policy-file=policy.yml
+pearmut run --port 8003 --url https://pearmut.ngrok.io
